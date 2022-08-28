0f5278d611a0521e5e1f3ac21b57a915f9cfa8ac
Manish Kumar <mnh2005124@gmail.com>
2022-08-29 07:08:38 +0800

    updates

--- variables/var.go
+++ variables/var.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 )
 
 var (
-	name, course string
-	module, clip int
+	name, course = "Manish", "Kaushik"
+	module, clip = "4", 2
 )
 
 func main() {
 	fmt.Println("Name and Course are set to ", name, "and", course, ".")
 	fmt.Println("Module and clip are set to", module, "and", clip, ".")
+	fmt.Println("Name is of type ", reflect.TypeOf(name))
+	fmt.Println("Module is of type ", reflect.TypeOf(module))
+	iMoudle, err := strconv.Atoi(module)
+	if err == nil {
+		total := iMoudle + clip
+		fmt.Println("Module plus clip equals", total)
+	}
 }
