c78e1e2faa26d3852ab0918d144d7a80b60fa070
Manish Kumar <mnh2005124@gmail.com>
2022-08-29 06:52:42 +0800

    variables details

---
+++ variables/var.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	name, course string
+	module, clip int
+)
+
+func main() {
+	fmt.Println("Name and Course are set to ", name, "and", course, ".")
+	fmt.Println("Module and clip are set to", module, "and", clip, ".")
+}
